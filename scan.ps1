48afe616b0e67268145bd5aedde2962e3a3b90fa
Caleb Stewart <caleb.stewart94@gmail.com>
2019-01-10 09:22:13 -0600

    Added UDP feature. Will scan UDP and TCP ports if requested.

--- scan.ps1
+++ scan.ps1
@@ -1,7 +1,10 @@
 param (
     [Parameter(Mandatory=$true)][IPAddress]$ip,
     [Parameter(Mandatory=$true)][IPAddress]$mask,
-    [Parameter(Mandatory=$true)][int[]]$ports
+    [int[]]$tcp = @(),
+    [int[]]$udp = @(),
+    [int]$timeout = 1000,
+    [int]$interval = 1000
 )
 
 # find start and end addresses
@@ -37,14 +40,51 @@ for( $addr = $start; $addr -lt $end; $addr++){
 
     Write-Progress -Activity "[scanning]" -Status "$($address.IPAddressToString): host is up" -PercentComplete ((($addr-$start)/($end-$start))*100)
 
-    # Test each port
-    foreach ( $port in $ports ) {
+    # Test each TCP port
+    foreach ( $port in $tcp ) {
         try {
             $socket = new-object System.Net.Sockets.TcpClient($address.IPAddressToString, $port)
             if( $socket.Connected ){
-                Write-Host "`r[$($address.IPAddressToString)] port $port is open"
+                Write-Host "`r[$($address.IPAddressToString)] tcp/$port is open"
                 $socket.Close()
             }
         } Catch {}
+    }
+
+    # Setup data to send on UDP
+    $data = [Text.Encoding]::ASCII.GetBytes("$(Get-Date)")
+
+    # Test each UDP port
+    foreach ( $port in $udp ){
+        # Open a connection, setup the timeout and send our data
+        $socket = new-object System.Net.Sockets.UdpClient($address.IPAddressToString, $port)
+        $socket.client.ReceiveTimeout = $timeout
+        [void]$socket.Send($data, $data.length)
+
+        # Setup an endpoint to receive responses
+        $recv = new-object System.Net.IPEndPoint([System.Net.IPAddress]::Any, 0)
+
+        try {
+            # Attempt to receive the data within the timeout
+            $received = $socket.Receive([ref]$recv)
+            [string]$receivedData = [Text.Encoding]::ASCII.GetString($received)
+            # If we received data, the port is open
+            if ( $receivedData ) {
+                Write-Host "[$($address.IPAddressToString)] udp/$port is open"
+            }
+        } Catch {
+            # The response was not rejected, it timed out. The port _may_ be open
+            # If there was an exception, and it didn't time out, then it was likely
+            # rejected with an ICMP packet. This means the port is closed.
+            if ( $Error[0].ToString() -match "\bRespond after a period of time\b" ) {
+                Write-Host "[$($address.IPAddressToString)] udp/$port is open|filtered"
+            }
+        }
+
+        # Close the socket
+        $socket.Close()
+
+        # Packets lost due to collision
+        Start-Sleep -Milliseconds $interval
     }
 }
